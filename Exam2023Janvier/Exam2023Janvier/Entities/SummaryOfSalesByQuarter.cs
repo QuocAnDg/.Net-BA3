0deb7f58fcce1603cd3072c2206a7933660d4c5b
QuocAnDg <quocand40@gmail.com>
2024-01-12 09:18:04 +0100

    demo legume

---
+++ Exam2023Janvier/Exam2023Janvier/Entities/SummaryOfSalesByQuarter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exam2023Janvier.Entities
+{
+    public partial class SummaryOfSalesByQuarter
+    {
+        public DateTime? ShippedDate { get; set; }
+        public int OrderId { get; set; }
+        public decimal? Subtotal { get; set; }
+    }
+}
